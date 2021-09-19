82786c1169116708f77a5993d30137fed61b3d6b
innaponomareva <innap85@gmail.com>
2021-09-19 04:18:43 +0200

    add Start page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/Navigation';
 import VerticalList from './components/VerticalList';
 import FilteredPhotos from './pages/FilteredPhotos';
 import MyPhotos from './pages/MyPhotos';
+import Start from './pages/Start';
 import UploadPhoto from './pages/UploadPhoto';
 
 
@@ -19,6 +20,7 @@ function App() {
           <Route path="/upload" exact component={UploadPhoto} />
           <Route path="/myphotos/:id" component={FilteredPhotos} />
           <Route path="/myphotos" exact component={MyPhotos} />
+          <Route path="/" exact component={Start} />
         </Switch>
       </div>
     </div>
---
+++ src/pages/Start.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/index.css'
+
+const Start = () => {
+
+  return (
+    <div className="start-text">Welcome to my photo gallery project!</div>
+  )
+}
+
+
+export default Start;
